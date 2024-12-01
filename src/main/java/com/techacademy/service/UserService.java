ee43f5fba1c7a1b4499600b6e0d4abf57a1ab185
miponta <miyuponco@gmail.com>
2024-12-01 22:45:45 +0900

    Lesson18Chapter8,9

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,11 +23,22 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    //----追加ここから-----
+    /**Userを1件検索して返す*/
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
     /**Userの登録を行う*/
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
+    //*Userの削除を行う*/
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id:idck) {
+            userRepository.deleteById(id);
+        }
+    }
 }
+
